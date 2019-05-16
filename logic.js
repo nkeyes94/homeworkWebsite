df2d36cd46be5efdabd62f5c66dd88f5a7ede804
Nathan Keyes <n.keyes1994@gmail.com>
2019-05-15 21:24:25 -0400

    Pushing code with working JS buttons instead of links

---
+++ logic.js
@@ -0,0 +1,14 @@
+$("#aboutMe").on("click", function()
+{
+    window.location="index.html";
+})
+
+$("#portfolioButton").on("click", function()
+{
+    window.location="portfolio.html";
+})
+
+$("#contactButton").on("click", function()
+{
+    window.location="contact.html";
+})
